1ec757159998aca516cef5ebcf2a65df534d0bea
Eike, Stefan <stefan.eike@ipdynamics.de>
2018-08-13 14:24:14 +0200

    Update Travis script

--- travis.sh
+++ travis.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 # Copyright 2017, 2018. DOCTALES. All rights reserved.
 # See the accompanying LICENSE file for applicable license.
+
+# DITA-OT Version
+VERSION="3.0.4"
+
 zip -r org.doctales.schematron.zip . -x *.zip* *.git/* *temp/* *out/*
-curl -LO 'https://github.com/dita-ot/dita-ot/releases/download/3.0.4/dita-ot-3.0.4.zip'
-unzip -q dita-ot-3.0.4.zip
-chmod +x dita-ot-3.0.4/bin/dita
-chmod +x dita-ot-3.0.4/bin/ant
-dita-ot-3.0.4/bin/dita --install https://github.com/doctales/org.doctales.ant-contrib/archive/master.zip
-dita-ot-3.0.4/bin/dita --install org.doctales.schematron.zip
+curl -LO 'https://github.com/dita-ot/dita-ot/releases/download/$VERSION/dita-ot-$VERSION.zip'
+unzip -q dita-ot-$VERSION.zip
+chmod +x dita-ot-$VERSION/bin/dita
+chmod +x dita-ot-$VERSION/bin/ant
+dita-ot-$VERSION/bin/dita --install https://github.com/doctales/org.doctales.ant-contrib/archive/master.zip
+dita-ot-$VERSION/bin/dita --install org.doctales.schematron.zip
 BASEDIR=$(dirname "$0")
 echo "Start Ant test"
-dita-ot-3.0.4/bin/ant -f build_test.xml -Ddita.dir=dita-ot
+dita-ot-$VERSION/bin/ant -f build_test.xml -Ddita.dir=dita-ot
 echo "Start DITA-OT test"
-dita-ot-3.0.4/bin/dita --input samples/sample.ditamap --format pdf2 -Dschematron.topic.validation.files=../rules/topic-validation.sch,../rules/topic-terminology.sch -DfailOnError.fatal=true -DfailOnError.error=true --verbose
+dita-ot-$VERSION/bin/dita --input samples/sample.ditamap --format pdf2 -Dschematron.topic.validation.files=../rules/topic-validation.sch,../rules/topic-terminology.sch -DfailOnError.fatal=true -DfailOnError.error=true --verbose
